ce9edd9c3e2a98aa1bc839e7f8f1f22052ff5fcd
Chandra Govind Rajan <cgovindrajan@gmail.com>
2017-10-05 01:09:35 -0400

    Adding functionality for 2D and imperfect2D

--- lib/processor.ex
+++ lib/processor.ex
@@ -84,9 +84,65 @@ defmodule Processor do
         end
     end
 
+    def build_neigbours_for_2D_topology(pid_neighbors_list_map,pid_list,index,imperfect,length,size) do
+
+         if(index == length) do
+            pid_neighbors_list_map
+        else
+            val = get_neigbours_for_2D_topology(index, size, pid_list, imperfect)
+            pid_neighbors_list_map = Map.put(pid_neighbors_list_map,Enum.at(pid_list,index),val)
+            index = index + 1
+            build_neigbours_for_2D_topology(pid_neighbors_list_map,pid_list,index,imperfect,length,size)
+        end
+   
+    end
+
+    def get_neigbours_for_2D_topology(index,size,pid_list,imperfect) do
+        row = div(index,size)
+        col = rem(index,size)
+            
+        row_level_neighbours = 
+                cond do
+                    col == 0 -> [Enum.at(pid_list,index+1)]
+                    col == size - 1 -> [Enum.at(pid_list,index - 1)]
+                    true ->  [Enum.at(pid_list, index - 1), Enum.at(pid_list, index + 1)]       
+                end
+
+            column_leve_neighbours = 
+                cond do
+                    row == 0 -> [Enum.at(pid_list,index+size)]
+                    row == size - 1 -> [Enum.at(pid_list,index - size)]
+                    true ->  [Enum.at(pid_list, index - size), Enum.at(pid_list, index + size)]       
+                end
+            
+            complete_neighbour = 
+                if !imperfect do
+                    row_level_neighbours ++ column_leve_neighbours
+                #Map.put(map,val,row_level_neighbours ++ column_leve_neighbours)
+                else
+                    complete_neighbour = row_level_neighbours ++ column_leve_neighbours
+                    random_neighbour = get_random_neighbour(pid_list,complete_neighbour)
+                    complete_neighbour ++ random_neighbour
+                #Map.put(map,val,complete_neighbour ++ random_neighbour)
+                end
+
+        complete_neighbour
+    end 
+
+    def get_random_neighbour(list,complete_neighbour) do
+        Enum.take_random(list -- complete_neighbour,1)
+    end
+
+    def clean_pid_list_for_2D_topology(pid_list) do
+        length = length(pid_list)
+        size = round(:math.sqrt(length))
+        # Drop records to make the list perfect 2D
+        list = Enum.drop(pid_list,-(length- (size*size)))
+    end
+
     def create_topology(topology, pid_list) do
         pid_neighbors_list_map = Map.new
-
+        IO.puts topology
         case topology do
             "full" ->
                 pid_neighbors_list_map = 
@@ -94,6 +150,15 @@ defmodule Processor do
             "line" ->
                 pid_neighbors_list_map = 
                     get_neighbors_for_line_topology(pid_neighbors_list_map, pid_list, 0)
+            "2D" -> 
+                pid_neighbors_list_map = 
+                build_neigbours_for_2D_topology(pid_neighbors_list_map,pid_list,0,false,length(pid_list),round(:math.sqrt(length(pid_list))))
+                    #get_neigbours_for_2D_topology(pid_neighbors_list_map, pid_list, 0,false)
+            "imp2D" ->
+                pid_neighbors_list_map = 
+                build_neigbours_for_2D_topology(pid_neighbors_list_map,pid_list,0,true,length(pid_list),round(:math.sqrt(length(pid_list))))
+                    #get_neigbours_for_2D_topology(pid_neighbors_list_map, pid_list, 0,true)
+
         end
     end
 
@@ -109,8 +174,11 @@ defmodule Processor do
             case algorithm do
                 "gossip" ->
                     pid_list_for_gossip = get_pid_list_for_gossip(num_of_nodes, [], self())
+                    if topology == "2D" || topology == "imp2D" do
+                        pid_list_for_gossip = clean_pid_list_for_2D_topology(pid_list_for_gossip)
+                    end
                     pid_neighbors_list_map = create_topology(topology, pid_list_for_gossip)
-
+                    
                     for {k,v} <- pid_neighbors_list_map do
                         IO.puts(inspect(k) <> " -> " <> inspect(v))
                     end
